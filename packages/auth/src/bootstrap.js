8ee1643e5d621889d3bf03c7609fec70dde2f6da
teddy <teddygirma20@gmail.com>
2025-05-05 15:31:49 +0300

    added auth subapp

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -4,7 +4,7 @@ import App from './App';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 
 // Mount function to startup the app
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+const mount = (el, {onSignIn, onNavigate, defaultHistory, initialPath }) => {
     const history = defaultHistory || createMemoryHistory({
         initialEntries: [initialPath],
     });
@@ -13,7 +13,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
         history.listen(onNavigate);
     }
     ReactDOM.render(
-      <App  history={history}/>,
+      <App onSignIn={onSignIn}  history={history}/>,
        el
     )
 
@@ -31,7 +31,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 
 // If we are in dev mode and running in isolation, call the bootstrap immediately
 if (process.env.NODE_ENV === 'development') {
-    const devRoot = document.querySelector('#_marketing-dev-root');
+    const devRoot = document.querySelector('#_auth-dev-root');
     
     if (devRoot) {
         mount(devRoot, { defaultHistory: createBrowserHistory() });
--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,8 +1,8 @@
-import { mount } from 'marketing/MarketingApp';
+import { mount } from 'auth/AuthApp';
 import React, { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
-export default () => {
+export default ({ onSignIn }) => {
     const ref = useRef(null);
     const history = useHistory();
 
@@ -11,7 +11,7 @@ export default () => {
         // Mount the marketing app to the div reference
         // This is where the marketing app will be rendered
      const { onParentNavigate } =  mount(ref.current, {
-          initialPath: history.location.pathname,
+           initialPath: history.location.pathname,
             onNavigate:({ pathname: nextPathname }) => {
                 const { pathname } = history.location;
                 if (pathname !== nextPathname) {
@@ -19,7 +19,8 @@ export default () => {
                     // to the new path
                     history.push(nextPathname)
                 }
-            }
+            },
+            onSignIn,
         });
 
         history.listen(onParentNavigate);
