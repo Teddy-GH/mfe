692c275f6f89fcae1b003a957230ce186b1cbc1d
teddy <teddygirma20@gmail.com>
2025-05-05 12:14:26 +0300

    added muli navigation with browser history and  memory history

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,27 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
     const ref = useRef(null);
+    const history = useHistory();
+
+
     useEffect(() => {
         // Mount the marketing app to the div reference
         // This is where the marketing app will be rendered
-        mount(ref.current);
+     const { onParentNavigate } =  mount(ref.current, {
+            onNavigate:({ pathname: nextPathname }) => {
+                const { pathname } = history.location;
+                if (pathname !== nextPathname) {
+                    // We are not on the same path, so we need to navigate
+                    // to the new path
+                    history.push(nextPathname)
+                }
+            }
+        });
+
+        history.listen(onParentNavigate);
     }, []);
 
     return <div ref={ref} />;
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
 // Mount function to startup the app
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+    
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
     ReactDOM.render(
-      <App />,
+      <App  history={history}/>,
        el
     )
+
+    return {
+        onParentNavigate({ pathname: nextPathname }) {
+            const { pathname } = history.location;
+            if (pathname !== nextPathname) {
+                // We are not on the same path, so we need to navigate
+                // to the new path
+                history.push(nextPathname);
+            }   
+        }
+    }
 }
 
 // If we are in dev mode and running in isolation, call the bootstrap immediately
 if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root');
+    
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
     }
 }
 
